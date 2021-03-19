881f3507c6983942d30a8c68ceed155b69a98fbe
Iorgen Wildrik Souza da Silva <iorgen.silva@sysmap.com.br>
2021-03-19 11:50:39 -0300

    feat: add table row buttons

--- src/app/components/product/product-read/product-read.component.ts
+++ src/app/components/product/product-read/product-read.component.ts
@@ -10,7 +10,7 @@ import { Component, OnInit } from '@angular/core';
 export class ProductReadComponent implements OnInit {
 
   products: IProduct[] = [];
-  displayedColumns: string[] = ['id', 'name', 'price'];
+  displayedColumns: string[] = ['id', 'name', 'price', 'action'];
 
   constructor(private productService: ProductService) { }
 
