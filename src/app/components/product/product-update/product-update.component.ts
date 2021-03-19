c63f98ba700793bbcd3566b6e50a16ec0c7b13ed
Iorgen Wildrik Souza da Silva <iorgen.silva@sysmap.com.br>
2021-03-19 13:57:22 -0300

    feat: add updateProduct method

--- src/app/components/product/product-update/product-update.component.ts
+++ src/app/components/product/product-update/product-update.component.ts
@@ -1,4 +1,7 @@
+import { IProduct } from './../product.model';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
+import { ProductService } from '../product.service';
 
 @Component({
   selector: 'app-product-update',
@@ -7,9 +10,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductUpdateComponent implements OnInit {
 
-  constructor() { }
-
-  ngOnInit(): void {
+  product: IProduct = {
+    id: null,
+    name: '',
+    price: null,
   }
 
+  constructor(
+    private productService: ProductService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) { }
+
+  ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('id');
+
+    if (!id) return;
+
+    this.productService.readById(id).subscribe(product => {
+      this.product = product
+    });
+  }
+
+  updateProduct(): void {
+    this.productService.update(this.product).subscribe(() => {
+      this.productService.showMessage('Produto Alterado com sucesso!');
+      this.router.navigate(['/products'])
+    });
+  }
+
+  cancel(): void {
+    this.router.navigate(['/products'])
+  }
 }
--- src/app/components/product/product.model.ts
+++ src/app/components/product/product.model.ts
@@ -1,5 +1,5 @@
 export interface IProduct {
-  id?: number;
+  id?: number | null;
   name: string;
   price?: number | null;
 }
